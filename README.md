2862e2f69e17d076b7497771025398d09e765cfc
wimuweb <wimuweb@gmail.com>
2024-07-25 21:18:02 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Cliente Gateway
+
+El gateway es el punto de comunicacion entre nuestros clientes y nuestros servicios, Es el encargado de recibir las peticiones, enviarlas a los servicios correspondientes y devolver las respuesta al cliente.
+
+## Dev
+
+1. Clonar el repositorio
+2. Instalar dependencias
+3. Crear un archivo `.env` basado en el `env.template`
+4. Tener levantados los microservicios que se van a consumir
+5. Ejecutar `npm run start:dev` o `node --run start:dev`
---
+++ src/products/products.controller.ts
@@ -0,0 +1,88 @@
+import {
+  // BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError, firstValueFrom } from 'rxjs';
+import { PaginationDto } from 'src/common';
+import { PRODUCT_SERVICE } from 'src/config';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(
+    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
+  ) {}
+
+  @Post()
+  createProduct(@Body() createProductDto: CreateProductDto) {
+    return this.productsClient.send(
+      { cmd: 'create-product' },
+      createProductDto,
+    );
+  }
+
+  @Get()
+  findAllProducts(@Query() paginationDto: PaginationDto) {
+    return this.productsClient.send(
+      { cmd: 'find-all-products' },
+      paginationDto,
+    );
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    this.productsClient.send({ cmd: 'find-one-product' }, { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+    // try {
+    //   const product = await firstValueFrom(
+    //     this.productsClient.send({ cmd: 'find-one-product' }, { id }),
+    //   );
+    //   return product;
+    // } catch (error) {
+    //   throw new RpcException(error);
+    // }
+  }
+
+  @Delete(':id')
+  deleteOne(@Param('id') id: string) {
+    return this.productsClient.send({ cmd: 'remove-product' }, { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+  }
+
+  @Patch(':id')
+  updateOne(
+    @Body() updateProductDto: UpdateProductDto,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.productsClient
+      .send(
+        { cmd: 'update-product' },
+        {
+          id,
+          ...updateProductDto,
+        },
+      )
+      .pipe(
+        catchError((err) => {
+          throw new RpcException(err);
+        }),
+      );
+  }
+}
